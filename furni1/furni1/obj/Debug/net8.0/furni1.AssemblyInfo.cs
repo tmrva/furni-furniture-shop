4ad4d14659b554fb611690d2d685ebb88145f1b9
Narmin Tomarova <tmrva621@gmail.com>
2024-07-12 14:56:37 +0400

    furni-furniture-shop

--- furni1/furni1/obj/Debug/net8.0/furni1.AssemblyInfo.cs
+++ furni1/furni1/obj/Debug/net8.0/furni1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("furni1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd43a3c686a7f937b017afe34bda6461a6dda1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3c9659de49458cbbd75364fcfc61e23ecab9b72")]
 [assembly: System.Reflection.AssemblyProductAttribute("furni1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("furni1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
